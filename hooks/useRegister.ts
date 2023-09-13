0fae19c0f207542ffcc4781c214ecd31e8642b86
Neat <sophaneat@student.fdu.edu>
2023-09-13 15:07:26 -0700

    Add AWS function to verify unregister user through verification code

--- hooks/useRegister.ts
+++ hooks/useRegister.ts
@@ -6,29 +6,48 @@ import {
 } from 'amazon-cognito-identity-js';
 
 const poolData: ICognitoUserPoolData = {
-    UserPoolId: process.env.AWS_CONGITO_POOL_ID!,
-    ClientId: process.env.AWS_CONGITO_CLIENT_ID!
+    UserPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_POOL_ID!,
+    ClientId: process.env.NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID!
 }
 const userPool = new CognitoUserPool(poolData);
 
-export const registerUser = (username: string, email: string, password:string) => {
-    const attributeList = [];
+export const registerUser = async (username: string, email: string, password:string) => {
 
-    const dataEmail = {
-        Name: "email",
-        Value: email
-    }
+    return new Promise((resolve, reject) => {
+        const attributeList = [];
 
-    const attributeEmail = new CognitoUserAttribute(dataEmail);
-
-    attributeList.push(attributeEmail);
-
-    userPool.signUp(username,password, attributeList, [], function(err, result) {
-        if (err) {
-            alert(err.message || JSON.stringify(err));
-            return;
+        const dataEmail = {
+            Name: "email",
+            Value: email
         }
-        var cognitoUser = result?.user;
-        console.log('user name is ' + cognitoUser?.getUsername());
+    
+        const attributeEmail = new CognitoUserAttribute(dataEmail);
+    
+        attributeList.push(attributeEmail);
+    
+        userPool.signUp(username,password, attributeList, [], function(err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                const cognitoUser = result?.user;
+                resolve(cognitoUser?.getUsername());
+            }
+        })
+    })
+}
+
+export const verifyUser = async(username: string, code: string) => {
+    return new Promise((resolve, reject) => {
+        const userData = {
+            Username: username,
+            Pool: userPool
+        };
+        const cognitoUser = new CognitoUser(userData);
+        cognitoUser.confirmRegistration(code, true, function(err, result) {
+            if (err) {
+                reject(err);
+            }
+            resolve(result);
+        });
     })
 }
--- pages/register-page/index.tsx
+++ pages/register-page/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
     FormControl,
     FormLabel,
@@ -8,14 +8,16 @@ import {
     InputGroup,
     InputRightElement,
     Stack,
-    Button
+    Button,
+    Text
 } from "@chakra-ui/react";
 import { EmailIcon } from "@chakra-ui/icons";
 import styles from "./register-page.module.scss";
-// AWS sign up function
-import { registerUser } from "@/hooks/useRegister";
+// AWS functions
+import { registerUser, verifyUser } from "@/hooks/useRegister";
 
 const RegisterPage = () => {
+    // State
     const [username, setUsername] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [pwd, setPwd] = useState<string>("");
@@ -23,99 +25,161 @@ const RegisterPage = () => {
     const [show1, setShow1] = useState<boolean>(false);
     const [show2, setShow2] = useState<boolean>(false);
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [usernameAWS, setUsernameAWS] = useState<string>("");
+    const [code, setCode] = useState<string>("");
 
+    const [formError, setFormError] = useState<string>("");
+    const [verificationError, setVerificationError] = useState<string>("");
+    // Form function
     const handleClick = () => setShow1(!show1);
     const handleClick2 = () => setShow2(!show2);
     
     // Handle Submitting
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async(e: React.FormEvent) => {
         e.preventDefault();
+
+        // Check if all required fields are filled
+        if(!username || !email || !pwd || !confirmPwd) {
+            setFormError("Missing required information");
+            return;
+        }
+        // Check if password matched confirm password
+        if(pwd !== confirmPwd) {
+            setFormError("Your password does not match the confirm password.");
+            return;
+        }
         setIsLoading(true);
-        console.log(username)
-        console.log(email)
-        console.log(pwd)
-        console.log(confirmPwd)
-        setTimeout(() => {
-            setIsLoading(false)
-        }, 1500)
+        const data: any = await registerUser(username, email, pwd);
+        setUsernameAWS(data);
+        setIsLoading(false);
     }
+    // Handle Verify
+    const handleVerify =  async(e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const result = await verifyUser(usernameAWS, code);
+            console.log(result);
+        } catch(error) {
+            setVerificationError("Your verification code is invalid or expired.")
+        }
+    }
+    
+    // Clear errors when user starts typing
+    useEffect(() => {
+        setFormError("");
+    }, [username, pwd, confirmPwd, email])
+    useEffect(() => {
+        setVerificationError("");
+    }, [code])
+
     return (
         <section className={styles.register}>
-            <h1 className={styles.register__title}>Register with Marketowns</h1>
-            {/* Register Form */}
-            <form onSubmit={handleSubmit} className={styles.register__form}>
-                <Stack spacing={4}>
-                    <FormControl isRequired>
-                        <FormLabel fontSize="sm">Username</FormLabel>
-                        <Input 
-                            value={username}
-                            onChange={(e)=>setUsername(e.target.value)}
-                            variant='flushed' 
-                            type='text' 
-                            size="sm" 
-                        />
-                    </FormControl>
-                    <FormControl isRequired>
-                        <FormLabel fontSize="sm">Email address</FormLabel>
-                        <Input 
-                            value={email}
-                            onChange={(e)=>setEmail(e.target.value)}
-                            variant='flushed' 
-                            type='email' 
-                            size="sm"
-                        />
-                    </FormControl>
-                    <FormControl isRequired>
-                        <FormLabel fontSize="sm">Password</FormLabel>
-                        <InputGroup size='md'>
-                            <Input
-                                pr='4.5rem'
-                                variant='flushed' 
-                                type={show1 ? 'text' : 'password'}
-                                value={pwd}
-                                onChange={(e)=>setPwd(e.target.value)}
+            {
+                !usernameAWS.length ? (
+                    <>
+                    <h1 className={styles.register__title}>Register with Marketowns</h1>
+                    {/* Register Form */}
+                    <form onSubmit={handleSubmit} className={styles.register__form}>
+                        {formError && <Text mb="5" fontSize="sm" color="red">{formError}</Text>}
+                        <Stack spacing={4}>
+                            <FormControl isRequired>
+                                <FormLabel fontSize="sm">Username</FormLabel>
+                                <Input 
+                                    value={username}
+                                    onChange={(e)=>setUsername(e.target.value)}
+                                    variant='flushed' 
+                                    type='text' 
+                                    size="sm" 
                             />
-                            <InputRightElement width='4.5rem'>
-                                <Button h='1.75rem' size='xs' onClick={handleClick}>
-                                {show1 ? 'Hide' : 'Show'}
-                                </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl isRequired>
-                        <FormLabel fontSize="sm">Confirm Password</FormLabel>
-                        <InputGroup size='md'>
-                            <Input
-                                pr='4.5rem'
-                                variant='flushed' 
-                                type={show2 ? 'text' : 'password'}
-                                value={confirmPwd}
-                                onChange={(e)=>setConfirmPwd(e.target.value)}
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel fontSize="sm">Email address</FormLabel>
+                                <Input 
+                                    value={email}
+                                    onChange={(e)=>setEmail(e.target.value)}
+                                    variant='flushed' 
+                                    type='email' 
+                                    size="sm"
+                                />
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel fontSize="sm">Password</FormLabel>
+                                <InputGroup size='md'>
+                                    <Input
+                                        pr='4.5rem'
+                                        variant='flushed' 
+                                        type={show1 ? 'text' : 'password'}
+                                        value={pwd}
+                                        onChange={(e)=>setPwd(e.target.value)}
+                                    />
+                                    <InputRightElement width='4.5rem'>
+                                        <Button h='1.75rem' size='xs' onClick={handleClick}>
+                                        {show1 ? 'Hide' : 'Show'}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel fontSize="sm">Confirm Password</FormLabel>
+                                <InputGroup size='md'>
+                                    <Input
+                                        pr='4.5rem'
+                                        variant='flushed' 
+                                        type={show2 ? 'text' : 'password'}
+                                        value={confirmPwd}
+                                        onChange={(e)=>setConfirmPwd(e.target.value)}
+                                    />
+                                    <InputRightElement width='4.5rem'>
+                                        <Button h='1.75rem' size='xs' onClick={handleClick2}>
+                                        {show2 ? 'Hide' : 'Show'}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            {
+                                isLoading ? (
+                                    <Button
+                                        isLoading
+                                        loadingText='Loading'
+                                        colorScheme='teal'
+                                        variant='outline'
+                                        spinnerPlacement='end'
+                                    ></Button>
+                                ): (
+                                    <Button onClick={handleSubmit} mt="5" colorScheme='teal' size='sm'>
+                                        Register
+                                    </Button>
+                                )
+                            }
+                        </Stack>
+                    </form>
+                    </>
+                ) : (
+                    <>
+                        <h1 className={styles.register__title}>Verification Step</h1>
+                        {/* Verifying registerd user via SMS */}
+                        <form className={styles.register__form}>
+                            <FormControl isRequired>
+                                <FormLabel fontSize="sm">Verification Code</FormLabel>
+                                <Input 
+                                    value={code}
+                                    onChange={(e)=>setCode(e.target.value)}
+                                    variant='flushed' 
+                                    type='text' 
+                                    size="sm" 
                             />
-                            <InputRightElement width='4.5rem'>
-                                <Button h='1.75rem' size='xs' onClick={handleClick2}>
-                                {show2 ? 'Hide' : 'Show'}
-                                </Button>
-                            </InputRightElement>
-                        </InputGroup>
-                    </FormControl>
-                    {
-                        isLoading ? (
-                            <Button
-                                isLoading
-                                loadingText='Loading'
-                                colorScheme='teal'
-                                variant='outline'
-                                spinnerPlacement='end'
-                            ></Button>
-                        ): (
-                            <Button onClick={handleSubmit} mt="5" colorScheme='teal' size='sm'>
-                                Register
+                            </FormControl>
+                            {verificationError && <Text mt="5" fontSize="sm" color="red">{verificationError}</Text>}
+                            <Button w="100%" onClick={handleVerify} mt="5" colorScheme='teal' size='sm'>
+                                Verify
                             </Button>
-                        )
-                    }
-                </Stack>
-            </form>
+                            
+                        </form>
+                    </>
+                )
+            }
+            
+            
         </section>
     )
 }
